f605d3afc98e643aa7b0a3fdb484b1d117e6f961
lbernhardt <lbernhardt2@gmail.com>
2019-04-12 11:24:12 -0300

    LoginPage

---
+++ Lands/Lands/Infraestructure/InstanceLocator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lands.Infraestructure
+{
+	using ViewModels;
+	class InstanceLocator
+    {
+		#region properties
+		public MainViewModel Main
+		{
+			get;
+			set;
+		}
+		#endregion
+
+		#region constructors
+		//Página login conectada a MainViewModel
+		public InstanceLocator()
+		{
+			this.Main = new MainViewModel();
+		}
+
+		#endregion
+	}
+}
